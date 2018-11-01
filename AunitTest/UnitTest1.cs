9c31fdd8556fbefb42c7543f0d358c98315fd957
Nir Malik <34807633+nirmalik@users.noreply.github.com>
2018-11-01 22:30:15 +0200

    UniTest and Packages
    all the UniTest and Packages

---
+++ AunitTest/UnitTest1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROJECT_19;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace AunitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Users user = new Users();
+        List<CourseDetails> course = new List<CourseDetails>();
+        //users
+        public int id=304801467;
+        public string firstName="barak";
+        public string lastName="avasker";
+        public string userName="barakav1";
+        public string password="123456";
+        public string mail="barakav1@asd.co.il";
+        public string address="dimona";
+        public int phone=123456;
+        public string job="student";
+        [TestMethod]
+        public void A_Testbuildcourse()
+        {
+           
+            Tools tool = new Tools(user);
+            Assert.IsTrue(tool.buildCourse(course));
+
+        }
+        [TestMethod]
+        public void A_testSetFirstName()
+        {
+            user.set_first_name(firstName);
+            Assert.IsTrue(firstName == user.get_first_name());
+        }
+        [TestMethod]
+        public void A_testSetLastName()
+        {
+            user.set_last_name(lastName);
+            Assert.IsTrue(lastName == user.get_last_name());
+        }
+        [TestMethod]
+        public void A_testSetPassword()
+        {
+            user.set_Password(password);
+            Assert.IsTrue(password == user.get_Password());
+        }
+        [TestMethod]
+        public void A_testSetmail()
+        {
+            user.set_mail(mail);
+            Assert.IsTrue(mail == user.get_mail());
+        }
+        [TestMethod]
+        public void A_testSetaddress()
+        {
+            user.set_address(address);
+            Assert.IsTrue(address == user.get_address());
+        }
+        [TestMethod]
+        public void A_testSetphone()
+        {
+            user.set_phone(phone);
+            Assert.IsTrue(phone == user.get_phone());
+        }
+        [TestMethod]
+        public void A_testSetjob()
+        {
+            user.set_job(job);
+            Assert.IsTrue(job == user.get_job());
+        }
+        //courses
+        CourseDetails courses = new CourseDetails();
+        public string start="08:00";
+        public string end="10:00";
+        public string name= "C language";
+        public string type= "lecture";
+        public string day= "Tuesday   ";
+        public string index="1";
+        public string approved="true";
+        public string semester="A1";
+        public int courseid=1000;
+        public int blocked=1006;
+        [TestMethod]
+        public void A_testSetstart()
+        {
+            courses.setSetStart(start);
+            Assert.IsTrue(start == courses.setGetStart());
+        }
+        [TestMethod]
+        public void A_testSetEnd()
+        {
+            courses.setSetend(end);
+            Assert.IsTrue(end == courses.setGetend());
+        }
+        [TestMethod]
+        public void A_testSetCoursename()
+        {
+            courses.setname(name);
+            Assert.IsTrue(name == courses.setGetname());
+        }
+        [TestMethod]
+        public void A_testSettype()
+        {
+            courses.setSettype(type);
+            Assert.IsTrue(type == courses.setGettype());
+        }
+        [TestMethod]
+        public void A_testSetday()
+        {
+            courses.setSetday(day);
+            Assert.IsTrue(day == courses.setGetday());
+        }
+        [TestMethod]
+        public void A_testSetindex()
+        {
+            courses.setSetindex(index);
+            Assert.IsTrue(index == courses.setGetindex());
+        }
+        [TestMethod]
+        public void A_testSetapproved()
+        {
+            courses.setSetapproved(approved);
+            Assert.IsTrue(approved == courses.setGetapproved());
+        }
+        [TestMethod]
+        public void A_testSetsemester()
+        {
+            courses.setSetsemester(semester);
+            Assert.IsTrue(semester == courses.setGetsemester());
+        }
+        [TestMethod]
+        public void A_testSetcourseid()
+        {
+            courses.setSetcourseid(courseid);
+            Assert.IsTrue(courseid == courses.setGetcourseid());
+        }
+        [TestMethod]
+        public void A_testSetblocked()
+        {
+            courses.setSetblocked(blocked);
+            Assert.IsTrue(blocked == courses.setGetblocked());
+        }
+
+        //getGrades
+
+        public int Id = 1;
+        public int studentId = 323602345;
+        public int CourseId = 1000;
+        public int score = 56;
+        GiveGrades grades = new GiveGrades();
+
+        [TestMethod]
+        public void A_testSetId()
+        {
+            grades.setId(Id);
+            Assert.IsTrue(Id == grades.getId());
+        }
+        [TestMethod]
+        public void A_testSetstudentId()
+        {
+            grades.setstudentId(studentId);
+            Assert.IsTrue(studentId == grades.getstudentId());
+        }
+        [TestMethod]
+        public void A_testSetCourseId()
+        {
+            grades.setCourseId(CourseId);
+            Assert.IsTrue(CourseId == grades.getCourseId());
+        }
+        [TestMethod]
+        public void A_testSetscore()
+        {
+            grades.setscore(score);
+            Assert.IsTrue(score == grades.getscore());
+        }
+
+    }
+    
+}
---
+++ BTEST/UnitTest1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using PROJECT_19;
+using THEUSERS;
+using StudentSpace;
+using Courses;
+using Connections;
+using System.Collections.Generic;
+
+namespace BTEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string firstName = "abed";
+        string lastName = "assa";
+        string password = "123456";
+        string userName = "abed12";
+        string address = "B12";
+        string mail = "abedab1@ac.sce.ac.il";
+        int phone = 0547957951;
+        Student student = new Student();
+        [TestMethod]
+        public void Test_Instance()
+        {
+
+            Assert.IsNotNull(Student.ins);
+        }
+
+        [TestMethod]
+        public void Test_firstName()
+        {
+        
+            student.setFirstName(firstName);
+            Assert.IsTrue(firstName == student.getFirstName());
+        }
+
+        [TestMethod]
+        public void Test_lastName()
+        {
+            student.setLastName(lastName);
+            Assert.IsTrue(lastName == student.getLastName());
+        }
+
+        [TestMethod]
+        public void Test_Password()
+        {
+            student.setPassword(password);
+            Assert.IsTrue(password == student.getPassword());
+        }
+        [TestMethod]
+        public void Test_UserName()
+        {
+            student.setUserName(userName);
+            Assert.IsTrue(userName == student.getUserName());
+        }
+
+        [TestMethod]
+        public void Test_Address()
+        {
+            student.setAddress(address);
+            Assert.IsTrue(address == student.getAddress());
+        }
+
+        [TestMethod]
+        public void Test_Phone()
+        {
+            student.setPhoneNumber(phone);
+            Assert.IsTrue(phone == student.getPhoneNumber());
+        }
+        [TestMethod]
+        public void Test_mail()
+        {
+            student.setMail(mail);
+            Assert.IsTrue(mail == student.getMail());
+        }
+
+        [TestMethod]
+        public void Test_Job()
+        {
+
+            Assert.IsTrue(Job.Student == student.getJob());
+        }
+
+        [TestMethod]
+        public void Test_getJobString()
+        {
+
+            Assert.IsTrue("Student" == student.getJobString());
+        }
+        [TestMethod]
+        public void Test_Symester()
+        {
+            student.setSemester("A");
+            Assert.IsTrue("A" == student.getSemester());
+        }
+
+        //Courses
+        Course cors = new Course();
+        int id = 1000;
+        string name = "Project";
+        int passScore = 56;
+        int blockCourseId = 1001;
+        double points = 3.5;
+        CourseType type = CourseType.Lecture;
+        int teacherId = 308306216;
+        string semester = "A";
+        string time = "10:12";
+        string day = "Sunday";
+
+        [TestMethod]
+        public void Test_courseId()
+        {
+            cors.setId(id);
+            Assert.IsTrue(id == cors.getId());
+
+        }
+
+        [TestMethod]
+        public void Test_courseName()
+        {
+            cors.setName(name);
+            Assert.IsTrue(name == cors.getName());
+
+        }
+
+        [TestMethod]
+        public void Test_passScore()
+        {
+            cors.setPassScore(passScore);
+            Assert.IsTrue(passScore == cors.getPassScore());
+
+        }
+
+        [TestMethod]
+        public void Test_blockCourse()
+        {
+            cors.setBlockCourse(blockCourseId);
+            Assert.IsTrue(blockCourseId == cors.getBlockCourseId());
+
+        }
+
+        [TestMethod]
+        public void Test_point()
+        {
+            cors.setPoints(points);
+            Assert.IsTrue(points == cors.getPoints());
+
+        }
+
+        [TestMethod]
+        public void Test_type()
+        {
+            cors.setType(type);
+            Assert.IsTrue(type == cors.getType());
+
+        }
+
+        [TestMethod]
+        public void Test_teacherId()
+        {
+            cors.setTeacherId(teacherId);
+            Assert.IsTrue(teacherId == cors.getTeacherId());
+
+        }
+
+        [TestMethod]
+        public void Test_courseSemester()
+        {
+            cors.setSemester(semester);
+            Assert.IsTrue(semester == cors.getSemester());
+
+        }
+
+        [TestMethod]
+        public void Test_time()
+        {
+            cors.setTime(time);
+            Assert.IsTrue(time == cors.getTime());
+
+        }
+        [TestMethod]
+        public void Test_courseDay()
+        {
+            cors.setDay(day);
+            Assert.IsTrue(day == cors.getDay());
+
+        }
+
+        //DataBases
+        string value = "abedab1";
+        string coulmn = "userName";
+
+        [TestMethod]
+        public void Test_getUser()
+        {
+            DBConnect.connect();
+            User s = DBConnect.selectUser(coulmn, value);
+            Assert.IsNotNull(s);
+        }
+
+        [TestMethod]
+        public void Test_getUserNameCheck()
+        {
+            DBConnect.connect();
+            User s = DBConnect.selectUser(coulmn, value);
+            Assert.IsTrue("Abed" == s.getFirstName());
+        }
+        [TestMethod]
+        public void Test_coursesList()
+        {
+            DBConnect.connect();
+            List<Course> s = DBConnect.getAllCourses();
+            Assert.IsNotNull(s);
+        }
+
+    }
+}
